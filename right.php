54e10dfd43168464d5deaf16a9d77a12f992fadc
KonstantinShirmanov <45903009+KonstantinShirmanov@users.noreply.github.com>
2020-05-31 22:59:33 +0300

    Create right.php

---
+++ right.php
@@ -0,0 +1,55 @@
+<div id="right">
+    
+    <div id="info"> 
+    <img src="CSS.png" align="left">
+    <p>  CSS используется создателями веб-страниц для задания цветов, шрифтов, 
+	расположения отдельных блоков и других аспектов представления внешнего вида 
+	этих веб-страниц. Основной целью разработки CSS являлось разделение описания 
+	логической структуры веб-страницы (которое производится с помощью HTML или 
+	других языков разметки) от описания внешнего вида этой веб-страницы (которое 
+	теперь производится с помощью формального языка CSS). Такое разделение может 
+	увеличить доступность документа, предоставить большую гибкость и возможность 
+	управления его представлением, а также уменьшить сложность и повторяемость в 
+	структурном содержимом. </p>
+	</div>
+    
+    <div id="reg">
+    
+    <form method="POST">
+		<div align="center">
+		<table border="0" width="90%" id="table2">
+			<tr>
+				<td colspan="3">
+		<p align="center">Расчет выражений</p></td>
+			</tr>
+			<tr>
+				<td width="26%"><span lang="ru">Первое число</span></td>
+				<td width="29%"><input type="text" name="T7" size="20"></td>
+				<td width="41%">Тип расчета</td>
+			</tr>
+			<tr>
+				<td width="26%"><span lang="ru">Второе число</span></td>
+				<td width="29%"><input type="text" name="T8" size="20"></td>
+				<td width="41%"><select size="1" name="D2">
+				<option>сумма</option>
+				<option>среднее</option>
+				<option>максимальное</option>
+				<option>минимальное</option>
+				</select></td>
+			</tr>
+			<tr>
+				<td width="26%"><span lang="ru">Ответ</span></td>
+				<td width="29%"><input type="text" name="T9" size="20"></td>
+				<td width="41%">&nbsp;</td>
+			</tr>
+			<tr>
+				<td colspan="3">
+				<p align="center">
+				<input type="submit" value="Рассчитать" name="B3"></p></td>
+			</tr>
+		</table>
+		</div>
+	</form>
+	<p>&nbsp;</p>
+  </div>
+    </div>
